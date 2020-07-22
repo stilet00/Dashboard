e98232d5d14a7352f272c54f219b781d5a0f29c4
stilet00 <safroninanton@gmail.com>
2020-07-22 23:17:20 +0300

    untrack sass-cache

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 gulp.task('css', function () {
-    return gulp.src('style.css')
+    return gulp.src('source/style.css')
         .pipe(gulp.dest('./build'));
 });
 gulp.task('html', function () {
-    return gulp.src('index.html')
+    return gulp.src('source/index.html')
         .pipe(gulp.dest('./build'));
 });
 gulp.task('image', function () {
-    return gulp.src('images/*')
+    return gulp.src('source/images/*')
         .pipe(gulp.dest('./build/images'));
 });
 gulp.task('fonts', function () {
-    return gulp.src('fonts/*/*')
+    return gulp.src('source/fonts/*/*')
         .pipe(gulp.dest('./build/fonts'));
 });
 gulp.task('build', gulp.series(['css', 'html', 'image', 'fonts']))
