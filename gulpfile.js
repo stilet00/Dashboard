d069adb3166a61a4aee6f13695382afbc3003181
stilet00 <safroninanton@gmail.com>
2020-07-22 23:00:18 +0300

    Message

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,19 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
-gulp.task('css', function () { return gulp.src(‘./css/*.css') .pipe(gulp.dest(‘./build'));
+gulp.task('css', function () {
+    return gulp.src('style.css')
+        .pipe(gulp.dest('./build'));
 });
+gulp.task('html', function () {
+    return gulp.src('index.html')
+        .pipe(gulp.dest('./build'));
+});
+gulp.task('image', function () {
+    return gulp.src('images/*')
+        .pipe(gulp.dest('./build/images'));
+});
+gulp.task('fonts', function () {
+    return gulp.src('fonts/*/*')
+        .pipe(gulp.dest('./build/fonts'));
+});
+gulp.task('build', gulp.series(['css', 'html', 'image', 'fonts']))
