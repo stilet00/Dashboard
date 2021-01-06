138e95928946a29cdb14af7f9707fc80d09e56d4
stilet00 <safroninanton@gmail.com>
2021-01-07 00:23:29 +0200

    final

--- build/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const deleteButton = document.querySelector('#navigation');
-    const urlLogo = document.getElementById('id');
-    function buttonPress(event) {
-        urlLogo.innerHTML = 'NEW URL';
-});
-    deleteButton.addEventListener('click', buttonPress);
-}
